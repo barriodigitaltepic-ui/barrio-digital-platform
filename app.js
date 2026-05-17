6639c7e7a46021b1facdbd8e18671ee6d95bc917
barriodigitaltepic-ui <barriodigitaltepic@gmail.com>
2026-05-17 14:18:42 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,55 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
 
-  const app = document.getElementById("app");
+const app=document.getElementById("app");
 
-  app.innerHTML = `
-    <section class="hero">
-        <h1>Barrio Digital Platform</h1>
-        <p>Plataforma Escalable para Negocios Locales</p>
-        <button id="btnDemo">Ver Demos</button>
-    </section>
-  `;
+try{
 
-  document.getElementById("btnDemo").addEventListener("click", () => {
-      alert("Aquí iniciaremos el sistema modular de demos");
-  });
+const respuesta=await fetch("data/negocios.json");
+const data=await respuesta.json();
+
+let html=`
+<section class="hero">
+<div>
+<h1>Barrio Digital Platform</h1>
+<p>Plataforma Escalable para Negocios Locales</p>
+</div>
+</section>
+
+<section class="grid">
+`;
+
+data.negocios.forEach(n=>{
+
+html+=`
+<div class="card">
+
+<img src="${n.imagen}">
+
+<h2>${n.nombre}</h2>
+
+<p>${n.descripcion}</p>
+
+<a href="?negocio=${n.slug}">
+<button style="background:${n.color}">
+Ver Demo
+</button>
+</a>
+
+</div>
+`;
+
+});
+
+html+=`</section>`;
+
+app.innerHTML=html;
+
+}catch(error){
+
+app.innerHTML="<h1>Error cargando plataforma</h1>";
+
+console.error(error);
+
+}
 
 });
