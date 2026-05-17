ed733d917736ada4e259d415852decdd65b45199
barriodigitaltepic-ui <barriodigitaltepic@gmail.com>
2026-05-17 14:07:07 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+  const app = document.getElementById("app");
+
+  app.innerHTML = `
+    <section class="hero">
+        <h1>Barrio Digital Platform</h1>
+        <p>Plataforma Escalable para Negocios Locales</p>
+        <button id="btnDemo">Ver Demos</button>
+    </section>
+  `;
+
+  document.getElementById("btnDemo").addEventListener("click", () => {
+      alert("Aquí iniciaremos el sistema modular de demos");
+  });
+
+});
